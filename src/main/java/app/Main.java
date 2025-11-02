d54958be4d2e12420c59ad812a30b4ec9f33e5b7
Alt+F₄raday <112538706+F4rqday@users.noreply.github.com>
2025-11-02 20:36:49 +0600

    feat(app): Main prints SCCs, topo order, SP/LP results + metrics

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,6 +10,7 @@ import java.util.List;
 import graph.common.BasicMetrics;
 import graph.common.Graph;
 import graph.common.Loader;
+import graph.dagsp.DagShortestPath;
 import graph.scc.CondensationBuilder;
 import graph.scc.TarjanSCC;
 import graph.topo.KahnTopologicalOrder;
@@ -41,5 +42,35 @@ for (int i=0;i<C;i++) System.out.println(" #"+i+" size="+groups.get(i).size()+" 
 
 
 
+Graph dag = CondensationBuilder.build(g, comp);
+System.out.println("Condensation DAG: n="+dag.n+", edges="+dag.edges.size());
 
+
+
+var topo = KahnTopologicalOrder.order(dag, m);
+System.out.println("Topo order of components: "+topo);
+
+
+int srcComp = comp[src];
+var sp = DagShortestPath.shortest(dag, srcComp, topo, m);
+System.out.println("Shortest dist from component "+srcComp+": "+Arrays.toString(sp.dist));
+
+int target = -1; long best = Long.MAX_VALUE;
+for (int i=0;i<dag.n;i++) if (sp.dist[i] < best) { best = sp.dist[i]; target = i; }
+System.out.println("One shortest path example to component "+target+": "+DagShortestPath.reconstruct(sp.parent, target));
+
+
+var lp = DagShortestPath.longestPath(dag, topo);
+System.out.println("Critical path length: "+lp.length);
+System.out.println("Critical path comp nodes: "+DagShortestPath.reconstruct(lp.parent, lp.end));
+
+
+m.stopTimer();
+System.out.println("Metrics: sccDfsVisited="+m.get("sccDfsVisited")+
+", sccEdges="+m.get("sccEdges")+
+", topoPushes="+m.get("topoPushes")+
+", topoPops="+m.get("topoPops")+
+", dagRelaxations="+m.get("dagRelaxations")+
+", elapsedMs="+m.elapsedMillis());
+}
 }
